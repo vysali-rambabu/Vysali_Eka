3d34cfa74d08bfdc257e20543f3cb6d3988409e3
vysali <vysalirambabu94@gmail.com>
2021-06-25 18:26:10 +0530

    Added a new function

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,31 @@
-print('Hello')
-i = 5
-print(i)
+import socket
 
-data = {}
 
-for i in data:
-    print(i)
-print('Done..')
+class Sample:
+
+    def __init__(self, name):
+        self.name = name
+
+    def result(self):
+        print('Invoke result()')
+        self.display('Hello', self.print_msg)
+
+    def display(self, data, func):
+        print('Invoke display()')
+        data = 'Hello'
+
+    def result_show(self):
+        pass
+
+    def print_msg(data):
+        print('Invoke print_msg()')
+        print(data)
+
+
+s = Sample('hi')
+s.result()
+
+s = '<deal>I am good.</deal></QML>'
+print(s[s.index('<deal>') + 6:s.index('</deal>')])
+
+print(socket.gethostname())
